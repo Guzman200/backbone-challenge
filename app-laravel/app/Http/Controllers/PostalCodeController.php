a3b6cb3f9ba7baef0c94afaf1696bc6ebbef4d52
Pedro Guzman <my.rg.developer@gmail.com>
2023-01-12 05:53:16 -0600

    Deleted tildes

--- app-laravel/app/Http/Controllers/PostalCodeController.php
+++ app-laravel/app/Http/Controllers/PostalCodeController.php
@@ -30,25 +30,25 @@ class PostalCodeController extends Controller
                 
                 $codeData = [
                     'zip_code' => $code,
-                    'locality' => mb_strtoupper($data[5]),
+                    'locality' => $this->eliminar_tildes(mb_strtoupper($data[5])),
                     'federal_entity' => [
                         'key'  => (int) $data[7],
-                        'name' => mb_strtoupper($data[4]),
+                        'name' => $this->eliminar_tildes(mb_strtoupper($data[4])),
                         'code' => null
                     ],
                     'settlements' => [],
                     'municipality' => [
                         'key'  => (int) $data[11],
-                        'name' => mb_strtoupper($data[3])
+                        'name' => $this->eliminar_tildes(mb_strtoupper($data[3]))
                     ]
                 ];
 
                 $settlements[] = [
                     'key'       => (int) $data[12],
-                    'name'      => mb_strtoupper($data[1]),
-                    'zona_type' => mb_strtoupper($data[13]),
+                    'name'      => $this->eliminar_tildes(mb_strtoupper($data[1])),
+                    'zona_type' => $this->eliminar_tildes(mb_strtoupper($data[13])),
                     'settlement_type' => [
-                        'name' => $data[2]
+                        'name' => $this->eliminar_tildes($data[2])
                     ]
                 ];
 
@@ -73,6 +73,44 @@ class PostalCodeController extends Controller
 
         return response()->json($codeData);
 
+    }
+
+    private function eliminar_tildes($cadena){
+    
+        //Ahora reemplazamos las letras
+        $cadena = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $cadena
+        );
+    
+        $cadena = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C'),
+            $cadena
+        );
+    
+        return $cadena;
     }
     
 }
